3cf1c5828c258c3727b51ec269ab5faf9db6a89c
SFR_BOX <cueille@protonmail.com>
2019-10-10 15:58:31 +0200

    Save BNF

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ft_fill(char *s, char c, char **res)
+{
+    int i;
+    int x;
+    int l;
+    
+    x = 0;
+    i = 0;
+    l = 0;
+    while(*s)
+    {
+        i = 0;
+        while(*s == c)
+            s++;
+        while(*s != c && *s)
+        {
+            res[l][i] = *s;
+            s++;
+            i++;
+        }
+        res[l][i] = '\0';
+        l++;
+    }
+}
+
+void ft_countchar(char *s, char c, char **res)
+{
+    int x;
+
+    while(*s)
+    {
+        x = 0;
+        while(*s == c)
+            s++;
+
+        while(*s != c && *s)
+        {
+            s++;
+            x++;
+        }
+        *res = malloc(x + 1);
+        res++;
+    }
+}
 int ft_countword(char *s, char c)
 {
     int i;
@@ -15,16 +59,19 @@ int ft_countword(char *s, char c)
             s++;
         
     }
+
 return (i);
 }
 
 char **ft_strsplit(char *s, char c)
 {
     char **res;
+    char i;
 
+    i = 0;
     res = malloc(sizeof(*res) * ft_countword(s,c));
-    
-
+    ft_countchar(s, c, res);
+    ft_fill(s, c, res);
     return (res);
 }
 int main()
