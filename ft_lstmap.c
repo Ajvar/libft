fd61e87acc345ce8d9bacb4409aa34a8b4849681
Joscelyn CUEILLE <jcueille@e3r8p5.42.fr>
2019-10-23 14:29:28 +0200

    final

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jcueille <jcueille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 15:22:47 by jcueille          #+#    #+#             */
-/*   Updated: 2019/10/23 10:51:59 by jcueille         ###   ########.fr       */
+/*   Updated: 2019/10/23 12:19:20 by jcueille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,17 @@ t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
 	t_list	*maillon;
-	t_list	*tmp;
 
 	res = NULL;
-	while (lst && f && del)
+	while (lst && f)
 	{
-		maillon = ft_lstnew(f(lst->content));
-		if (!maillon)
-			return(NULL);
+		if (!(maillon = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&res, maillon);
-		tmp = lst->next;
-		ft_lstdelone(lst, del);
-		free(lst);
-		lst = tmp;
+		lst = lst->next;
 	}
 	return (res);
 }
